207ab67d2ffb1276957f49d2c3f2c64fd176808e
appcreatorguy <manmenonsense@gmail.com>
2019-07-31 21:15:08 +0100

    Finished page 1 and 2 css

---
+++ html/step-2.php
@@ -0,0 +1,106 @@
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Libora - Save your money and Barter!</title>
+
+        <link rel="stylesheet" href="css/1140.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+
+        
+    </head>
+    <body>
+        <div class="container12">
+            <header>
+                <div class="row">
+                    <div class="column7" id="header-nav">
+                        <a href="https://libora.000webhostapp.com"><img src="images/logo.png" alt="Libora logo"></a>
+                    </div>
+
+                    <div class="column5">
+                        <ul>
+                            <li><a href="/login">Login Here</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+
+            <h1 id="home"><strong>Great!</strong> Just one more step..</h1>
+
+            <form action="" method="post">
+                <div class="alert"></div>
+                <div class="row">
+                    <div class="column3">
+                        <input type="text" id="" class="large-fld" name="email" value="" placeholder="Email Address">
+                    </div>
+                    <div class="column3">
+                        <input type="text" id="" class="large-fld" name="city" value="" placeholder="City">
+                    </div>
+                    <div class="column3">
+                        <select name="state" class='large-fld'>
+                            <option value="AL">Alabama</option>
+                            <option value="AK">Alaska</option>
+                            <option value="AZ">Arizona</option>
+                            <option value="AR">Arkansas</option>
+                            <option value="CA">California</option>
+                            <option value="CO">Colorado</option>
+                            <option value="CT">Connecticut</option>
+                            <option value="DE">Delaware</option>
+                            <option value="FL">Florida</option>
+                            <option value="GA">Georgia</option>
+                            <option value="HI">Hawaii</option>
+                            <option value="ID">Idaho</option>
+                            <option value="IL">Illinois</option>
+                            <option value="IN">Indiana</option>
+                            <option value="IA">Iowa</option>
+                            <option value="KS">Kansas</option>
+                            <option value="KY">Kentucky</option>
+                            <option value="LA">Louisiana</option>
+                            <option value="ME">Maine</option>
+                            <option value="MD">Maryland</option>
+                            <option value="MA">Massachusetts</option>
+                            <option value="MI">Michigan</option>
+                            <option value="MN">Minnesota</option>
+                            <option value="MS">Mississippi</option>
+                            <option value="MO">Missouri</option>
+                            <option value="MT">Montana</option>
+                            <option value="NE">Nebraska</option>
+                            <option value="NV">Nevada</option>
+                            <option value="NH">New Hampshire</option>
+                            <option value="NJ">New Jersey</option>
+                            <option value="NM">New Mexico</option>
+                            <option value="NY">New York</option>
+                            <option value="NC">North Carolina</option>
+                            <option value="ND">North Dakota</option>
+                            <option value="OH">Ohio</option>
+                            <option value="OK">Oklahoma</option>
+                            <option value="OR">Oregon</option>
+                            <option value="PA">Pennsylvania</option>
+                            <option value="RI">Rhode Island</option>
+                            <option value="SC">South Carolina</option>
+                            <option value="SD">South Dakota</option>
+                            <option value="TN">Tennessee</option>
+                            <option value="TX">Texas</option>
+                            <option value="UT">Utah</option>
+                            <option value="VT">Vermont</option>
+                            <option value="VA">Virginia</option>
+                            <option value="WA">Washington</option>
+                            <option value="WV">West Virginia</option>
+                            <option value="WI">Wisconsin</option>
+                            <option value="WY">Wyoming</option>
+                        </select>
+                    </div>
+                    <div class="column3">
+                        <input type="submit" value="Let's do this!" class="large-btn large-magnify" name="step2">
+                    </div>
+                </div>
+			</form>
+			
+			<p id="home-info">Seperate your wants and offers with a comma: <span>Baby sitter, washer fixed, cheap car</span></p>
+        </div>
+    </body>
+</html>
